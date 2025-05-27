e16bcb7fca4eda99863bf285881294f04480bf58
@fmarcos <@fmarcos.dev>
2025-05-27 12:48:36 -0400

    running on 3000

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("hello mundo");
+});
+
+app.listen(port, () => {
+  console.log(`running on http://localhost:${port}`);
+});
