d188830043fe08145d3dba79e26e6d71aeea859f
@fmarcos <@fmarcos.dev>
2025-05-27 14:00:56 -0400

    added typescript

--- server.ts
+++ server.ts
@@ -1,11 +1,12 @@
-const express = require("express");
+import express, { Request, Response } from "express";
+
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get("/", (_: Request, res: Response) => {
   res.send("hello mundo");
 });
 
